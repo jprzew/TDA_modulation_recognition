4208327911fb64a77697a4d075ec89c0a5bcf281
Janusz Przewocki <janusz.przewocki@ug.edu.pl>
2021-09-16 12:45:14 +0200

    Implemented a new features-framework (TestClass2). Still under test.

--- modurec/features.py
+++ modurec/features.py
@@ -1,7 +1,291 @@
+from inspect import getfullargspec
+
 import pandas as pd
 import numpy as np
 from modurec import pandex    # Necessary for the mr dataframe accessor
 from ripser import Rips
+from functools import partial
+
+df = pd.DataFrame({'A': [1, 2, 3]})
+
+
+@pd.api.extensions.register_dataframe_accessor('test')
+class TestClass:
+
+    def __init__(self, df):
+        self.df = df
+        self.features = []
+
+    def __getitem__(self, name):
+        return partial(getattr(TestClass, name),
+                       accessor=self)
+
+    class Feature:
+
+        def __new__(cls, *args, **kwargs):
+            instance = super(TestClass.Feature, cls).__new__(cls)
+            kwargs['accessor'].features.append(instance)
+            instance.accessor = kwargs['accessor']
+
+            return instance
+
+    class A(Feature):
+        a = None
+
+        def __init__(self, a, *args, **kwargs):
+            self.a = a
+
+        def compute(self):
+            return self.A
+
+    class B(Feature):
+
+        b = None
+
+        def __init__(self, a, b=7, accessor=None):
+            self.b = b
+
+            # super().__init__(a)
+            # df['B'] = self.compute()
+            # self.B = df.B
+
+        def compute(self):
+            return 2*self.A
+
+        # return TestClass.FeatureCreator(getattr(TestClass, name))
+
+@pd.api.extensions.register_dataframe_accessor('test2')
+class TestClass2:
+
+    def __init__(self, df):
+        self.df = df
+        self.features = []
+        self.rips = Rips()
+
+
+    def __getitem__(self, name):
+        return partial(getattr(TestClass, name),
+                       accessor=self)
+
+    def create_feature(self, name, **kwargs):
+
+        creator = getattr(TestClass2, name)
+        instance = creator(**kwargs)
+
+        self.features.append(instance)
+        instance.df = self.df
+        instance.creator = self
+
+        column_name = str(instance)
+        if column_name not in self.df.columns:
+            df[str(instance)] = instance.compute()
+
+        return instance
+
+    def get_values(self, feature):
+
+        return self.df[str(feature)]
+
+    class Feature:
+
+        def __str__(self):
+            keys = getfullargspec(self.__init__).args[1:]
+            values = [self.__dict__[key] for key in keys]
+
+            # values = [value for key, value in self.__dict__.items()
+            #           if key in keys]
+
+            string = self.__class__.__name__
+
+            if keys:
+                string += '_' + '={}_'.join(keys) + '={}'
+                string = string.format(*values)
+
+            #
+            # string = '={}_'.join(keys)
+            # string = self.__class__.__name__ + '_' + string
+            # string += '={}'
+
+            return string
+
+        def values(self):
+            return self.df[str(self)]
+
+    class A(Feature):
+
+        def __init__(self, a):
+            self.a = a
+
+        def compute(self):
+            return self.df['A']
+
+    class B(Feature):
+
+        a = None
+        b = None
+
+        def __init__(self, a, b=7):
+            self.a = a
+            self.b = b
+
+        def compute(self):
+            A = self.creator.create_feature('A', a=self.a)
+
+            return 2*A.values()
+
+
+    class signal_sample(Feature):
+
+        # def __init__(self):
+        #     pass
+
+        def compute(self):
+            return self.df['signal_sample']
+
+    class signal_sampleQ(Feature):
+
+        # def __init__(self):
+        #     pass
+
+        def compute(self):
+            return self.df['signal_sampleQ']
+
+    class point_cloud(Feature):
+
+        dim = None
+
+        def __init__(self, dim=2):
+            self.dim = dim
+
+        def compute(self):
+            def __compute2D(samples):
+                samples.mr.add_point_cloud(window=1)
+
+                return samples['point_cloud']
+
+            def __compute3D(samples):
+
+                def __standardize(x):
+                    range0 = np.max(x[:, 0]) - np.min(x[:, 0])
+                    range1 = np.max(x[:, 1]) - np.min(x[:, 1])
+                    range2 = np.max(x[:, 2]) - np.min(x[:, 2])
+                    x[:, 2] = x[:, 2] * 0.5 * (range0 + range1) / range2
+                    return x
+
+                samples.mr.add_point_cloud(window=1, point_cloud_col='cloud_3D')
+                samples['cloud_3D'] = \
+                    samples.cloud_3D.apply(lambda x:
+                                           np.column_stack([x, range(x.shape[0])]))
+                samples['cloud_3D'] = samples.cloud_3D.apply(__standardize)
+
+                return samples['cloud_3D']
+
+            def __compute4D(sample):
+                samples.mr.add_point_cloud(window=2, point_cloud_col='cloud_4D')
+                return samples['cloud_4D']
+
+            signal_sample = self.creator.create_feature('signal_sample')
+            signal_sampleQ = self.creator.create_feature('signal_sampleQ')
+
+            samples = pd.concat([signal_sample.values(),
+                                 signal_sampleQ.values()],
+                                axis=1)
+
+            if self.dim == 2:
+                return __compute2D(samples)
+            elif self.dim == 3:
+                return __compute3D(samples)
+            elif self.dim == 4:
+                return __compute4D(samples)
+
+            raise(NotImplemented('Dimension not implemented.'))
+
+    class diagram(Feature):
+
+        dim = None
+
+        def __init__(self, dim=2):
+            self.dim = dim
+
+        def compute(self):
+            point_cloud = self.creator.create_feature('point_cloud', dim=self.dim)
+            return point_cloud.values().map(self.creator.rips.fit_transform)
+
+    class H(Feature):
+
+        n = None
+        dim = None
+
+        def __init__(self, n, dim=2):
+            self.dim = dim
+            self.n = n
+
+        def compute(self):
+            diagram = self.creator.create_feature('diagram', dim=self.dim)
+
+            return pd.DataFrame(diagram.values().tolist(),
+                                    index=self.df.index)[self.n]
+
+    class life_time(Feature):
+
+        n = None
+        dim = None
+
+        def __init__(self, n, dim=2):
+            self.dim = dim
+            self.n = n
+
+        def compute(self):
+            homology = self.creator.create_feature('H',
+                                                   n=self.n,
+                                                   dim=self.dim)
+            return homology.values().np.diff(axis=1)
+
+    class no(Feature):
+
+        n = None
+        dim = None
+
+        def __init__(self, n, dim=2):
+            self.dim = dim
+            self.n = n
+
+        def compute(self):
+            homology = self.creator.create_feature('H',
+                                                   n=self.n,
+                                                   dim=self.dim)
+            return homology.values().map(lambda x: x.shape[0])
+
+    class mean(Feature):
+
+        n = None
+        dim = None
+
+        def __init__(self, n, dim=2):
+            self.dim = dim
+            self.n = n
+
+        def compute(self):
+            life_time = self.creator.create_feature('life_time',
+                                                    n=self.n,
+                                                    dim=self.dim)
+            return life_time.values().np.mean()
+
+    class var(Feature):
+
+        n = None
+        dim = None
+
+        def __init__(self, n, dim=2):
+            self.dim = dim
+            self.n = n
+
+        def compute(self):
+            life_time = self.creator.create_feature('life_time',
+                                                    n=self.n,
+                                                    dim=self.dim)
+            return life_time.values().np.var()
+
 
 
 @pd.api.extensions.register_dataframe_accessor('feat')
@@ -261,3 +545,36 @@ class SignalFeatures:
 
     def kmp_f5_H1_4D(self):
         return self.df.feat['kmp_features_H1_4D'].map(lambda x: x[4])
+
+
+if __name__ == '__main__':
+    df = pd.read_pickle('../data/testpickle.pkl')
+    homology0 = df.test2.create_feature('H', n=0, dim=2)
+    homology1 = df.test2.create_feature('H', n=1, dim=2)
+    life_time_0 = df.test2.create_feature('life_time', n=0, dim=2)
+    life_time_1 = df.test2.create_feature('life_time', n=1, dim=2)
+    no_0 = df.test2.create_feature('no', n=0, dim=2)
+    no_1 = df.test2.create_feature('no', n=1, dim=2)
+    mean_0 = df.test2.create_feature('mean', n=0, dim=2)
+    mean_1 = df.test2.create_feature('mean', n=1, dim=2)
+    var_0 = df.test2.create_feature('var', n=0, dim=2)
+    var_1 = df.test2.create_feature('var', n=1, dim=2)
+
+    assert(df['H_n=0_dim=2'].equals(df['H0']))
+    assert(df['H_n=1_dim=2'].equals(df['H1']))
+    assert(df['life_time_n=0_dim=2'].equals(df['H0_life_time']))
+    assert(df['life_time_n=1_dim=2'].equals(df['H1_life_time']))
+    assert(df['no_n=1_dim=2'].equals(df['no_H1']))
+    assert(df['no_n=0_dim=2'].equals(df['no_H0']))
+    assert(df['mean_n=1_dim=2'].equals(df['H1_mean']))
+    assert(df['mean_n=0_dim=2'].equals(df['H0_mean']))
+    assert(df['var_n=1_dim=2'].equals(df['H1_var']))
+    assert(df['var_n=0_dim=2'].equals(df['H0_var']))
+
+
+
+
+
+
+
+
