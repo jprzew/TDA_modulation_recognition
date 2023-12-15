e39c1f36398c78b770d3719f6257e03989a0dfb1
Janusz Przewocki <janusz@lifeconcept.tech>
2023-12-15 17:02:31 +0100

    Removed a commented section

--- src/modurec/features/point_cloud.py
+++ src/modurec/features/point_cloud.py
@@ -98,55 +98,6 @@ class PointCloud(Feature):
 
         return computer.handle(clouds=clouds, dim=self.dim, steps=steps, kind=self.kind)
 
-    #
-    # def compute(self):
-    #
-    #     df = self.creator.df['point_cloud']
-    #
-    #     if self.preproc == 'fft':
-    #         df = df.apply(self.fft_cloud)
-    #
-    #     if self.kind is None:
-    #         window = self.dim / 2
-    #     elif self.kind == 'abs':
-    #         def amplitude(point_cloud):
-    #             return np.apply_along_axis(lambda x: np.linalg.norm(x), 1, point_cloud)
-    #
-    #         df = df.apply(amplitude)
-    #         window = float(self.dim)
-    #     elif self.kind == 'phi':
-    #         def argument(point_cloud):
-    #             return np.apply_along_axis(lambda x: phase(x[0] + x[1] * 1j), 1, point_cloud)
-    #
-    #         df = df.apply(argument)
-    #         window = float(self.dim)
-    #
-    #     else:
-    #         raise NotImplemented('Point cloud type not implemented.')
-    #
-    #     if isinstance(self.step, str):
-    #         step_col = self.step
-    #         df = pd.concat([df, self.creator.df[step_col]], axis=1)
-    #     elif isinstance(self.step, int):
-    #         step_col = 'step'
-    #         df = pd.DataFrame(df)
-    #         df[step_col] = self.step
-    #     else:
-    #         raise ValueError('Parameter step need to be str or int')
-    #
-    #     if window == 1:
-    #         return df['point_cloud']
-    #
-    #     if window.is_integer():
-    #         return df.apply(lambda x: windowed_cloud(x['point_cloud'],
-    #                                                  window=int(window),
-    #                                                  step=x[step_col]), axis=1)
-    #     else:
-    #         if self.dim == 3:
-    #             return self.compute_3d(df)
-    #         else:
-    #             raise NotImplemented('Dimension not implemented.')
-
 
 class StandardCloudComputer(Computer):
     """Computes standard type of point cloud (kind = None)"""
@@ -222,33 +173,3 @@ class ReducedCloudComputer(Computer):
         else:
             return (pd.concat([clouds, steps], axis=1)
                     .apply(lambda x: windowed_cloud(x.iloc[0], window=int(window), step=x.iloc[1]), axis=1))
-
-
-
-######## Old point cloud to be tested against
-
-class PointCloud2(Feature):
-
-    def __init__(self, dim=2, step=1, kind=None, preproc=None):
-        self.dim = dim
-        self.step = step
-        self.kind = kind
-        self.preproc = preproc
-
-    @staticmethod
-    def compute_3d(df):
-
-        def standardize(x):
-            range0 = np.max(x[:, 0]) - np.min(x[:, 0])
-            range1 = np.max(x[:, 1]) - np.min(x[:, 1])
-            range2 = np.max(x[:, 2]) - np.min(x[:, 2])
-            x[:, 2] = x[:, 2] * 0.5 * (range0 + range1) / range2
-            return x
-
-        df['point_cloud'] = df.point_cloud.apply(lambda x:
-                                                 np.column_stack([x, range(x.shape[0])]))
-        df['point_cloud'] = df.point_cloud.apply(standardize)
-
-        return df['point_cloud']
-
-
