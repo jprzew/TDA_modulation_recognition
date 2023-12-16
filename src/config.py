454f4878680b63945b8cdfdbabb8a64d8f344b93
Janusz Przewocki <janusz@lifeconcept.tech>
2023-12-16 17:14:37 +0100

    First version of a script calculating diagrams

--- src/config.py
+++ src/config.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
 class General:
     hdf_data_file = 'data/GOLD_XYZ_OSC.0001_1024.hdf5'
 
@@ -13,8 +17,33 @@ class SampledData:
     cases_per_class = 100
 
 
+@dataclass
+class FeatureData:
+    name: str
+    params: dict
+
+
 class Diagrams:
-    pass
+    modulation_subset: list = ['BPSK', 'QPSK', '8PSK', '16PSK',
+                               '32PSK', '16QAM', '32QAM', '64QAM',
+                               'FM', 'GMSK', 'OQPSK']
+
+    to_calculate = [FeatureData(name='diagram', params={'dim': 2}),
+                    FeatureData(name='diagram', params={'dim': 3}),
+                    FeatureData(name='diagram', params={'dim': 4}),
+                    FeatureData(name='diagram', params={'dim': 10}),
+                    FeatureData(name='diagram', params={'dim': 2, 'kind': 'abs'}),
+                    FeatureData(name='diagram', params={'dim': 10, 'kind': 'abs'}),
+                    FeatureData(name='diagram', params={'dim': 2, 'kind': 'phi'}),
+                    FeatureData(name='diagram', params={'dim': 10, 'kind': 'phi'}),
+                    FeatureData(name='diagram', params={'dim': 2, 'kind': 'abs', 'fil': 'star'}),
+                    FeatureData(name='diagram', params={'dim': 2, 'kind': 'phi', 'fil': 'star'}),
+                    FeatureData(name='diagram', params={'dim': 2, 'step': 30}),
+                    FeatureData(name='diagram', params={'dim': 4, 'step': 30})]
+
+    sample_size: Optional[int] = 2  # sample size per modulation
+
+    diagrams_file = 'data/diagrams.pkl'
 
 
 class Features:
--- src/diagrams.py
+++ src/diagrams.py
@@ -1,2 +1,24 @@
-print('Calculating diagrams...')
+import config as cfg
+import pandas as pd
+from utils import get_repo_path
+import modurec.features  # Needed for feature factory to work
 
+df = pd.read_pickle(get_repo_path() / cfg.SampledData.sampled_data_file)
+
+# Subsample modulations and cases
+df = df[df.modulation_type.isin(cfg.Diagrams.modulation_subset)]
+
+if cfg.Diagrams.sample_size is not None:
+    df = df.groupby('modulation_type').apply(lambda x: x.sample(n=cfg.Diagrams.sample_size))
+
+# Calculate diagrams
+print('Calculating diagrams...')
+diagram_features = []
+for case, feature_data in enumerate(cfg.Diagrams.to_calculate):
+    print(f'Calculating {case+1}/{len(cfg.Diagrams.to_calculate)}')
+    print('------------------------')
+    diagram_features.append(df.ff.create_feature(feature_data.name, **feature_data.params))
+
+# Save to file
+to_take = list(map(str, diagram_features))
+df[to_take].to_pickle(get_repo_path() / cfg.Diagrams.diagrams_file)
