2ae1abf7cd029f5531f3dff8e8fc20af67e59d20
Janusz Przewocki <janusz@lifeconcept.tech>
2023-12-17 17:22:34 +0100

    Reacted on PerformanceWarning in featurize.py

--- src/featurize.py
+++ src/featurize.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import warnings
 from utils import get_repo_path
 import config as cfg
 from feature_list import all_features
@@ -17,7 +18,10 @@ results = []
 for case, feature_data in enumerate(all_features):
     print(f'Calculating {case+1}/{len(all_features)}. Feature data: {feature_data}')
     print('------------------------')
-    results.append(calculate_feature(df=diagrams, feature_data=feature_data))
+    with warnings.catch_warnings(record=True) as w:
+        results.append(calculate_feature(df=diagrams, feature_data=feature_data))
+        if (len(w) > 0) and (w[0].category == pd.errors.PerformanceWarning):  # In case of dataframe fragmentation
+            diagrams = diagrams.copy()
 
 # Save to file
 pd.concat(results, axis=1).to_pickle(get_repo_path() / cfg.Featurize.features_file)
