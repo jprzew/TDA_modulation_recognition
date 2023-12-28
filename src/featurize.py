c6a6606c3f578dde29df565e2ae5ae2078a0687a
Janusz Przewocki <janusz.przewocki@ug.edu.pl>
2023-12-28 14:53:49 +0100

    Created main; removed unused df

--- src/featurize.py
+++ src/featurize.py
@@ -6,22 +6,25 @@ from feature_list import all_features
 from modurec.features.feature import calculate_feature
 
 
-# Reading the data
-df = pd.read_pickle(get_repo_path() / cfg.SampleData.sampled_data_file)
+def main():
 
-# Reading the diagrams
-diagrams = pd.read_pickle(get_repo_path() / cfg.Diagrams.diagrams_file)
+    # Reading the diagrams
+    diagrams = pd.read_pickle(get_repo_path() / cfg.Diagrams.diagrams_file)
 
-# Calculate features
-print('Calculating features...')
-results = []
-for case, feature_data in enumerate(all_features):
-    print(f'Calculating {case+1}/{len(all_features)}. Feature data: {feature_data}')
-    print('------------------------')
-    with warnings.catch_warnings(record=True) as w:
-        results.append(calculate_feature(df=diagrams, feature_data=feature_data))
-        if (len(w) > 0) and (w[0].category == pd.errors.PerformanceWarning):  # In case of dataframe fragmentation
-            diagrams = diagrams.copy()
+    # Calculate features
+    print('Calculating features...')
+    results = []
+    for case, feature_data in enumerate(all_features):
+        print(f'Calculating {case+1}/{len(all_features)}. Feature data: {feature_data}')
+        print('------------------------')
+        with warnings.catch_warnings(record=True) as w:
+            results.append(calculate_feature(df=diagrams, feature_data=feature_data))
+            if (len(w) > 0) and (w[0].category == pd.errors.PerformanceWarning):  # In case of dataframe fragmentation
+                diagrams = diagrams.copy()
 
-# Save to file
-pd.concat(results, axis=1).to_pickle(get_repo_path() / cfg.Featurize.features_file)
+    # Save to file
+    pd.concat(results, axis=1).to_pickle(get_repo_path() / cfg.Featurize.features_file)
+
+
+if __name__ == '__main__':
+    main()
