6858d22751740d46fd011ce9bdd47df4e6f71f87
Janusz Przewocki <janusz.przewocki@ug.edu.pl>
2023-12-28 14:50:42 +0100

    Added main function in diagrams.py

--- src/diagrams.py
+++ src/diagrams.py
@@ -22,32 +22,38 @@ def calculate_feature_cached(feature_data):
     return calculate_feature(df=df, feature_data=feature_data)
 
 
-# Read data
-df = pd.read_pickle(get_repo_path() / cfg.SampleData.sampled_data_file)
+def main():
 
-# Subsample modulations and cases
-df = df[df.modulation_type.isin(cfg.Diagrams.modulation_subset)]
+    # Read data
+    df = pd.read_pickle(get_repo_path() / cfg.SampleData.sampled_data_file)
 
-# Filter by SNR
-if cfg.Diagrams.snr_threshold is not None:
-    df = df[df.SNR >= cfg.Diagrams.snr_threshold]
+    # Subsample modulations and cases
+    df = df[df.modulation_type.isin(cfg.Diagrams.modulation_subset)]
 
-# Sample cases
-if cfg.Diagrams.sample_size is not None:
-    df = (df.groupby(['modulation_type', 'SNR'], as_index=False)
-            .apply(lambda x: x.sample(n=cfg.Diagrams.sample_size))
-            .droplevel(0))
+    # Filter by SNR
+    if cfg.Diagrams.snr_threshold is not None:
+        df = df[df.SNR >= cfg.Diagrams.snr_threshold]
 
-# Calculate diagrams
-print('Calculating diagrams...')
-results = []
-for case, feature_data in enumerate(cfg.Diagrams.to_calculate):
-    print(f'Calculating {case+1}/{len(cfg.Diagrams.to_calculate)}. Feature data: {feature_data}')
-    print('------------------------')
-    results.append(calculate_feature_cached(feature_data=feature_data))
+    # Sample cases
+    if cfg.Diagrams.sample_size is not None:
+        df = (df.groupby(['modulation_type', 'SNR'], as_index=False)
+                .apply(lambda x: x.sample(n=cfg.Diagrams.sample_size))
+                .droplevel(0))
 
-# Save to file
-pd.concat(results, axis=1).to_pickle(get_repo_path() / cfg.Diagrams.diagrams_file)
+    # Calculate diagrams
+    print('Calculating diagrams...')
+    results = []
+    for case, feature_data in enumerate(cfg.Diagrams.to_calculate):
+        print(f'Calculating {case+1}/{len(cfg.Diagrams.to_calculate)}. Feature data: {feature_data}')
+        print('------------------------')
+        results.append(calculate_feature_cached(feature_data=feature_data))
 
-# Clean cache
-memory.clear(warn=False)
+    # Save to file
+    pd.concat(results, axis=1).to_pickle(get_repo_path() / cfg.Diagrams.diagrams_file)
+
+    # Clean cache
+    memory.clear(warn=False)
+
+
+if __name__ == '__main__':
+    main()
