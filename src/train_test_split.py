b04f2f2697a1c5b5893883b303ac3cde7209621c
Janusz Przewocki <janusz.przewocki@ug.edu.pl>
2023-12-28 14:59:09 +0100

    Added main function to train_test_split.py

--- src/train_test_split.py
+++ src/train_test_split.py
@@ -7,9 +7,14 @@ import numpy as np
 # Setting random seed
 np.random.seed(cfg.TrainTestSplit.random_seed)
 
-print('Splitting dataset into train/test subsets...')
-splitter = radioml_dataset.get_splitter(input_file=cfg.General.hdf_data_file)
-splitter.split(test_proportion=cfg.TrainTestSplit.test_proportion)
-splitter.save_to_file(train_output_file=cfg.TrainTestSplit.train_indices_file,
-                      test_output_file=cfg.TrainTestSplit.test_indices_file)
 
+def main():
+    print('Splitting dataset into train/test subsets...')
+    splitter = radioml_dataset.get_splitter(input_file=cfg.General.hdf_data_file)
+    splitter.split(test_proportion=cfg.TrainTestSplit.test_proportion)
+    splitter.save_to_file(train_output_file=cfg.TrainTestSplit.train_indices_file,
+                          test_output_file=cfg.TrainTestSplit.test_indices_file)
+
+
+if __name__ == '__main__':
+    main()
